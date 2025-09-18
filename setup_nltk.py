8f0b4489a3564806bc70c3242263b3f7c4c4597d
Shahariar Hossain Remon <87390549+SHRemon@users.noreply.github.com>
2025-09-18 16:18:21 +0600

    Add files via upload

---
+++ setup_nltk.py
@@ -0,0 +1,16 @@
+
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# Download required NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+print("NLTK setup complete!")
